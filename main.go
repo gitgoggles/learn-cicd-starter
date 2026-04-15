85c33843c3a35775753259f3ee6bcda46a082518
gitgoggles <ruckojr@hotmail.com>
2026-04-15 22:19:23 +0100

    added linting test

--- main.go
+++ main.go
@@ -24,7 +24,12 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+
+}
+
 func main() {
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
