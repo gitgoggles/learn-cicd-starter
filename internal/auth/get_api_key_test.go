dce2d0b3c2446769009145a4b80822cf38e5acf9
gitgoggles <ruckojr@hotmail.com>
2026-04-15 21:29:53 +0100

    broke code on purpose

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	header := make(http.Header)
+	header.Add("Authorization", "ApiKey supersecret")
+
+	got, _ := GetAPIKey(header)
+	want := "supersecret"
+
+	if reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
